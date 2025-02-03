4aa37bcab400b5a74543e2f0a0ebd9b500ca64a2
Ernesto Cruz <161363066+NetoCruzzz@users.noreply.github.com>
2025-02-03 05:26:54 +0000

    First commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,25 @@
+public class DataConversion {
+
+    public static void main(String[] args) {
+        // Conv. string into float;
+        String strFloat = "374.25";
+        float from_strFloat = Float.parseFloat(strFloat);
+
+        // Conv. string into integer;
+        String strInt = "1250";
+        int from_strInt = Integer.parseInt(strInt);
+
+        // Conv. float into integer;
+        float floatNum = 56.715f;
+        int from_floatNum = (int) floatNum;
+
+        // Conv. integer into float;
+        int intNum = 1500;
+        float from_intNum = (float) intNum;
+
+        // Conv. the double into string;
+        double doubleNum = 3.13149265;
+        String from_doubleNum = Double.toString(doubleNum);
+    }
+}
 
